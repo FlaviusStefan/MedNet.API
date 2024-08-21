5287bf52f4a0714c178a74db1ef291f255ee412c
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-08-21 10:22:32 +0300

    "CreatedAddressesAndContacts" + "AddedAddressesAndContactsToDoctor"

--- MedNet.API/Data/ApplicationDbContext.cs
+++ MedNet.API/Data/ApplicationDbContext.cs
@@ -15,8 +15,46 @@ namespace MedNet.API.Data
 
         public DbSet<Doctor> Doctors { get; set; }
         public DbSet<Patient> Patients { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
         public DbSet<Appointment> Appointments { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
 
+            modelBuilder.Entity<Address>()
+                .HasKey(a => a.Id);
+
+            modelBuilder.Entity<Address>()
+                .HasMany(a => a.Doctors)
+                .WithOne(d => d.Address)
+                .HasForeignKey(d => d.AddressId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Contact>()
+                .HasKey(c => c.Id);
+
+            modelBuilder.Entity<Contact>()
+                .HasMany(c => c.Doctors)
+                .WithOne(d => d.Contact)
+                .HasForeignKey(d => d.ContactId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Doctor>()
+                .HasKey(d => d.Id);
+
+            modelBuilder.Entity<Doctor>()
+                .HasOne(d => d.Address)
+                .WithMany(a => a.Doctors)
+                .HasForeignKey(d => d.AddressId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Doctor>()
+                .HasOne(d => d.Contact)
+                .WithMany(c => c.Doctors)
+                .HasForeignKey(d => d.ContactId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
--- MedNet.API/Models/Domain/Doctor.cs
+++ MedNet.API/Models/Domain/Doctor.cs
@@ -8,7 +8,15 @@
         public string Specialization { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Gender { get; set; }
-        
+        public string LicenseNumber { get; set; }
+        public int YearsOfExperience { get; set; }
+
+        public Guid AddressId { get; set; }
+        public Address Address { get; set; }
+
+        public Guid ContactId { get; set; }
+        public Contact Contact { get; set; }
+
 
     }
 }
